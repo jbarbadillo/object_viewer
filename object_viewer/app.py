2bf1da334299f3e1df68d0e5fffbffe71e9e4f8d
jbarbadillo <javier.barbadillo@gmail.com>
2019-01-27 22:09:08 +0100

    Fix linting

--- object_viewer/app.py
+++ object_viewer/app.py
@@ -29,8 +29,11 @@ class ObjectViewer:
 
         graph_source = ColumnDataSource(data)
         # Create figure
-        self.figure = figure(title='2d map', tools='pan,box_zoom,box_select,reset', plot_width=600, plot_height=600)
-        
+        self.figure = figure(title='2d map',
+                             tools='pan,box_zoom,box_select,reset',
+                             plot_width=600,
+                             plot_height=600)
+
         # Create plot
         self.graph_plot = self.figure.circle('x', 'y', source=graph_source, size=20, color='navy', alpha=0.6)
         return
@@ -39,16 +42,12 @@ if __name__ == "__main__":
     print("Started Object viewer {}".format(__version__ ))
 
     # output to static HTML file
-    output_file("line.html")    
+    output_file("line.html")
 
     circle_x = [1, 2]
     circle_y = [6, 7]
     square_x = [1, 2]
     square_y = [6.1, 7.1]
-    
+
     viewer = ObjectViewer()
     viewer.create_page()
-
-
-
-    
