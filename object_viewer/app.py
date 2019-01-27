720577a9a43a43188929f220a9cbb561b3d8253e
jbarbadillo <javier.barbadillo@gmail.com>
2019-01-27 18:56:43 +0100

    Refactor render method

--- object_viewer/app.py
+++ object_viewer/app.py
@@ -5,6 +5,12 @@
 from object_viewer.__init__ import __version__
 from bokeh.plotting import figure, output_file, show
 
+def render_plot(circle_x, circle_y, square_x, square_y):
+    p.circle_x(circle_x, circle_y, size=20, color="navy", alpha=0.5)
+    p.square(square_x, square_y, size=20, color="olive", alpha=0.5)
+
+    # show the results
+    show(p)
 
 if __name__ == "__main__":
     print("Started Object viewer {}".format(__version__ ))
@@ -14,14 +20,12 @@ if __name__ == "__main__":
 
     p = figure(plot_width=400, plot_height=400)
 
-    # add a circle renderer with a size, color, and alpha
-    circle_x = [1, 2, 3, 4, 5]
-    circle_y = [6, 7, 2, 4, 5]
-    square_x = [1, 2, 3, 4, 5]
-    square_y = [7, 8, 3, 5, 6]
-    p.circle_x(circle_x, circle_y, size=20, color="navy", alpha=0.5)
-    p.square(square_x, square_y, size=20, color="olive", alpha=0.5)
+    circle_x = [1, 2]
+    circle_y = [6, 7]
+    square_x = [1, 2]
+    square_y = [6.1, 7.1]
+    
+    render_plot(circle_x, circle_y, square_x, square_y)
 
 
-    # show the results
-    show(p)
+    
