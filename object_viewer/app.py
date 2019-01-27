fd10c954f61038102d28e01e89e21a933856732c
jbarbadillo <javier.barbadillo@gmail.com>
2019-01-27 16:34:31 +0100

    Add main module start

--- object_viewer/app.py
+++ object_viewer/app.py
@@ -1,3 +1,8 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+
+from object_viewer.__init__ import __version__
+
+if __name__ == "__main__":
+    print("Started Object viewer {}".format(__version__))
