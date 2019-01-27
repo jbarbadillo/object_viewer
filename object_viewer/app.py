3c48a64cb2be47d4e27be7611a089999b004cc75
jbarbadillo <javier.barbadillo@gmail.com>
2019-01-27 18:31:52 +0100

    Test figure plot

--- object_viewer/app.py
+++ object_viewer/app.py
@@ -9,4 +9,13 @@ from bokeh.plotting import figure, output_file, show
 if __name__ == "__main__":
     print("Started Object viewer {}".format(__version__ ))
 
-    
+    # output to static HTML file
+    output_file("line.html")
+
+    p = figure(plot_width=400, plot_height=400)
+
+    # add a circle renderer with a size, color, and alpha
+    p.circle([1, 2, 3, 4, 5], [6, 7, 2, 4, 5], size=20, color="navy", alpha=0.5)
+
+    # show the results
+    show(p)
