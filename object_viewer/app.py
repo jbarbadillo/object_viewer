49f3dc942dbedf8f2794ce0d108a142d280f5095
jbarbadillo <javier.barbadillo@gmail.com>
2019-01-27 18:29:16 +0100

    Import bokeh

--- object_viewer/app.py
+++ object_viewer/app.py
@@ -3,6 +3,10 @@
 """Main module."""
 
 from object_viewer.__init__ import __version__
+import bokeh.plotting
+
 
 if __name__ == "__main__":
-    print("Started Object viewer {}".format(__version__))
+    print("Started Object viewer {}".format(__version__ ))
+
+    
