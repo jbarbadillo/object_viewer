ed5191bf1ceff6c6440a548081f6910b0b8e1446
jbarbadillo <javier.barbadillo@gmail.com>
2019-02-01 19:49:49 +0100

    Reorder data source creation

--- object_viewer/app.py
+++ object_viewer/app.py
@@ -12,34 +12,15 @@ class ObjectViewer:
     """ Object renderer"""
 
     def __init__(self):
-        self.data = {'x': [],
-                     'y': [],
-                     'uid': []} # generate data with pandas cols?
-        self.graph_source = None
         self.figure = figure(x_range=(-2, 2), y_range=(-2, 2), toolbar_location=None)
         self.figure.border_fill_color = 'black'
-        self.graph_plot = None
-        self._display_graph()
 
-        # self.graph_plot.data_source.on_change('selected', self.graph_update)
+        # TODO: create glyph circle renderer
+        self.circle_renderer = self.figure.circle()
+        self.source = self.circle_renderer.data_source
 
-    def create_page(self):
-        show(self.figure)
+        # TODO: create a callback
 
-    def _display_graph(self):
-        # TODO: generate source using pandas and injecting data every second
-
-        self.graph_source = ColumnDataSource(self.data)
-
-
-        # Create plot
-        self.graph_plot = self.figure.circle('x', 'y', source=self.graph_source, size=20, color='navy', alpha=0.6,
-                                             line_color="#3288bd", line_width=3)
-
-    def update_source(self, new_data):
-        self.graph_source = ColumnDataSource(new_data)
-        self.graph_plot = self.figure.circle('x', 'y', source=self.graph_source, size=20, color='navy', alpha=0.6,
-                                             line_color="#3288bd", line_width=3)
 
 if __name__ == "__main__":
     print("Started Object viewer {}".format(__version__ ))
@@ -50,11 +31,9 @@ if __name__ == "__main__":
     data = {'x': [1],
             'y': [1],
             'uid': [1]}
-    viewer.update_source(data)
-    viewer.create_page()
+
 
     sleep(1)
     data = {'x': [1],
             'y': [1.1],
             'uid': [1]}
-    viewer.update_source(data)
